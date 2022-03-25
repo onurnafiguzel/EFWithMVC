2e12328e7d09ad6b7d3da901a5b16ba411ee72da
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2022-03-25 11:03:25 +0300

    Services ready

---
+++ Business/Abstract/IClassroomService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IClassroomService
+    {
+        public void Add(Classroom classroom);
+        public void Update(int id);
+        public void Delete(int id);
+        public List<Classroom> GetAll();
+    }
+}
---
+++ Business/Abstract/IStudentService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IStudentService
+    {
+        public void Add(Student student);
+        public void Update(int id);
+        public void Delete(int id);
+        public List<Student> GetAll();
+    }
+}
